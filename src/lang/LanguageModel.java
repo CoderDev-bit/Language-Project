1f15c815ba361ea186cff267b3af5a78bc17423e
shiva <shivamrpat@gmail.com>
2025-05-11 18:43:46 -0400

    null

--- src/lang/LanguageModel.java
+++ src/lang/LanguageModel.java
@@ -1,8 +1,22 @@
 package lang;
 
+import util.DatabaseManager;
+
 public class LanguageModel {
 
+    DatabaseManager db;
 
+    public LanguageModel(DatabaseManager db) {
+
+    }
+
+    public void train(String strLanguageName, String strText) {
+
+    }
+
+    public void detect(String strText){
+
+    }
 
 
 }
